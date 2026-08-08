0891bff9bb150043e3738fb5d0e655b7514e75e0
Sahil Yatam <149784395+SahilYatam@users.noreply.github.com>
2026-08-08 10:36:42 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stack = []
+        const map = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        };
+
+        for(let char of s){
+            if(char in map){
+                if(stack.length === 0){
+                    return false
+                }
+
+                let top = stack.pop()
+
+                if(top !== map[char]){
+                    return false
+                }
+
+            } else {
+                stack.push(char)
+            }
+        }
+        return stack.length === 0
+    }
+}
